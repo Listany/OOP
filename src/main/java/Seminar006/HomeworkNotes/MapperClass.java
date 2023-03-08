ca0164ae9410b2e7ffe9c54ae9aec606b1b031fa
Listratova Tatyana <listratova1@gmail.com>
2023-03-08 21:12:58 +0300

    Added Homework007

---
+++ src/main/java/Seminar006/HomeworkNotes/MapperClass.java
@@ -0,0 +1,12 @@
+public class MapperClass implements Mapper{
+    @Override
+    public String map(Note note) {
+        return String.format("%s, %s, %s", note.getId(), note.getHeader(), note.getText());
+    }
+
+    @Override
+    public Note map(String line) {
+        String [] lines = line.split(",");
+        return new Note(lines[0], lines[1], lines[2]);
+    }
+}
